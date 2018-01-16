6a4323088198e911d2c0138cf42911c3cd923533
Brooke Lewis <belv2c@gmail.com>
2018-01-15 20:50:20 -0600

    created some children

---
+++ LoopingConsole/Child.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopingConsole
+{
+    class Child // plain old c sharp object (poco) 
+    {
+        public string Name { get; set; }   // auto property
+        public bool Sick { get; set; }
+
+        public string ForgeASickNote() // method signature
+        {
+            return Sick
+              ?  $"Please excuse {Name} from class today because they are very sick." // ternary if
+              :  $"{Name}, you still have to go to school, quit faking. Love Mom"; // ternary else
+        }
+    }
+}
---
+++ LoopingConsole/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LoopingConsole
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+
+            // lists live in the collections.generic using statement (generic type)
+            // list of instances of the child class
+            // lists are classes and you can use object initializers with them
+            // object init inside of an object init 
+            var children = new List<Child>
+            { 
+                new Child {Name = "Suzie", Sick = false}, // only lets you name new properties (Name is property)
+                new Child {Name = "Ralph", Sick = true}
+            };
+
+            // for each instance of the child class inside of the list children - do this code
+            foreach (var child in children)
+            {
+                // if you hit a child whos name is Suzie, stop this iteration of the loop and move on to the next child
+                // aka skip and move onto next
+                if (child.Name == "Suzie")
+                {
+                    continue;
+                    // break; if i hit a kid named Suzie quit the loop and move on 
+                }
+                Console.WriteLine($"{child.Name} is {(child.Sick ? "sick" : "healthy")}");
+                Console.WriteLine(child.ForgeASickNote());
+            }
+
+
+            var lineIRead = Console.ReadLine();
+        }
+    }
+}
